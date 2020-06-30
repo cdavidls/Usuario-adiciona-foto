76182d2ce101c3cf6e75942c0a1fbc57f7575293
David <dcicero493@gmail.com>
2020-06-30 14:42:37 -0300

    usuario adiciona foto

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace UsuarioAdicionaFoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PtbFoto = new System.Windows.Forms.PictureBox();
+            this.BtnAbrir = new System.Windows.Forms.Button();
+            this.TxtNomeArquivo = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.PtbFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PtbFoto
+            // 
+            this.PtbFoto.Location = new System.Drawing.Point(58, 66);
+            this.PtbFoto.Name = "PtbFoto";
+            this.PtbFoto.Size = new System.Drawing.Size(160, 177);
+            this.PtbFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PtbFoto.TabIndex = 0;
+            this.PtbFoto.TabStop = false;
+            // 
+            // BtnAbrir
+            // 
+            this.BtnAbrir.Location = new System.Drawing.Point(76, 249);
+            this.BtnAbrir.Name = "BtnAbrir";
+            this.BtnAbrir.Size = new System.Drawing.Size(120, 42);
+            this.BtnAbrir.TabIndex = 1;
+            this.BtnAbrir.Text = "Abrir";
+            this.BtnAbrir.UseVisualStyleBackColor = true;
+            this.BtnAbrir.Click += new System.EventHandler(this.BtnAbrir_Click);
+            // 
+            // TxtNomeArquivo
+            // 
+            this.TxtNomeArquivo.Location = new System.Drawing.Point(69, 40);
+            this.TxtNomeArquivo.Name = "TxtNomeArquivo";
+            this.TxtNomeArquivo.Size = new System.Drawing.Size(136, 20);
+            this.TxtNomeArquivo.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 365);
+            this.Controls.Add(this.TxtNomeArquivo);
+            this.Controls.Add(this.BtnAbrir);
+            this.Controls.Add(this.PtbFoto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PtbFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PtbFoto;
+        private System.Windows.Forms.Button BtnAbrir;
+        private System.Windows.Forms.TextBox TxtNomeArquivo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsuarioAdicionaFoto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAbrir_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                TxtNomeArquivo.Text = openFileDialog1.FileName;
+
+                PtbFoto.ImageLocation = openFileDialog1.FileName;
+
+                PtbFoto.Load();
+            }
+        }
+    }
+}
